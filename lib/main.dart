ce32f7f6b1894c2edbaef24c153e21a169b48078
Krishn1412 <f20202093@hyderabad.bits-pilani.ac.in>
2022-06-24 22:23:16 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dialogflow/dialogflow_v2.dart';
+import 'model/chatModel.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Chatbot(),
+    );
+  }
+}
+class Chatbot extends StatefulWidget {
+  @override
+  _ChatbotState createState() => _ChatbotState();
+}
+
+class _ChatbotState extends State<Chatbot> {
+  List<ChatMessage> messages = [];
+  TextEditingController _inputMessageController = new TextEditingController();
+  late Dialogflow dialogflow;
+  late AuthGoogle authGoogle;
+  ScrollController _scrollController = new ScrollController(
+    initialScrollOffset: 0.0,
+    keepScrollOffset: true,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration.zero, () async {
+      await initiateDialogFlow();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _scrollToBottom();
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(249, 239, 238, 10),
+
+      body: Stack(
+          children:[
+            Center(
+              child:CircleAvatar(
+                backgroundImage: AssetImage('assets/masti.png'),
+                backgroundColor: Colors.transparent,
+                radius:40.0,
+              ),
+            ),
+            SizedBox(height: 40.0),
+            Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            chatSpaceWidget(),
+            Container(
+              height: 1.0,
+              width: double.infinity,
+              color: Colors.blueGrey,
+            ),
+            bottomChatView()
+          ],
+        ),
+      ],
+    ),
+    );
+  }
+
+  Widget chatSpaceWidget() {
+    return Flexible(
+      child: SingleChildScrollView(
+        controller: _scrollController,
+        physics: BouncingScrollPhysics(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            ListView.builder(
+              itemCount: messages.length,
+              shrinkWrap:true,
+              padding: EdgeInsets.only(top: 10,bottom: 10),
+              physics: NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index){
+                return Container(
+                  padding: EdgeInsets.only(left: 14,right: 14,top: 10,bottom: 10),
+                  child: Align(
+                    alignment: (messages[index].messageType == "receiver"?Alignment.topLeft:Alignment.topRight),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: (messages[index].messageType  == "receiver"?Colors.grey.shade200:Color.fromRGBO(227, 207, 201, 30)),
+                      ),
+                      padding: EdgeInsets.all(16),
+                      child: Text(messages[index].messageContent, style: TextStyle(fontSize: 15),),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget bottomChatView() {
+    return Container(
+      padding: EdgeInsets.only(left: 10, bottom: 10, top: 10),
+      height: 60,
+      width: double.infinity,
+      child: Row(
+        children: <Widget>[
+          SizedBox(
+            width: 15,
+          ),
+          Expanded(
+            child: TextField(
+              controller: _inputMessageController,
+              onSubmitted: (String str) {
+                fetchFromDialogFlow(str);
+              },
+              decoration: InputDecoration(
+                  hintText: "Write message...",
+                  hintStyle: TextStyle(color: Colors.black54),
+                  border: InputBorder.none),
+            ),
+          ),
+          SizedBox(
+            width: 15,
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              fetchFromDialogFlow(_inputMessageController.text);
+            },
+            child: Icon(
+              Icons.send,
+              color: Colors.white,
+              size: 18,
+            ),
+            backgroundColor: Colors.brown,
+            elevation: 0,
+          ),
+        ],
+      ),
+    );
+  }
+
+  _scrollToBottom() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (_scrollController.hasClients) {
+        _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
+      }
+    });
+  }
+
+  initiateDialogFlow() async {
+    AuthGoogle authGoogle =
+    await AuthGoogle(fileJson: 'assets/chatbotkey.json').build();
+    dialogflow = Dialogflow(authGoogle: authGoogle, language: Language.english);
+  }
+
+  fetchFromDialogFlow(String input) async {
+    _inputMessageController.clear();
+    setState(() {
+      messages.add(ChatMessage(messageContent: input, messageType: "sender"));
+    });
+
+    AIResponse response = await dialogflow.detectIntent(input);
+    print(response.getMessage());
+    messages.add(ChatMessage(
+        messageContent: response.getMessage(), messageType: "receiver"));
+    setState(() {});
+  }
+}
