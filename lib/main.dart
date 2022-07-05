ad198bcc33b7818cab8505fa39903304e0e53904
mr-harshtyagi <writetoharshtyagi@gmail.com>
2022-07-05 09:54:26 +0530

    custom payload feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,22 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_dialogflow/dialogflow_v2.dart';
 import 'model/chatModel.dart';
 
-
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Chatbot(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Chatbot(),
     );
   }
 }
@@ -50,18 +50,17 @@ class _ChatbotState extends State<Chatbot> {
   Widget build(BuildContext context) {
     _scrollToBottom();
     return Scaffold(
-      backgroundColor: Color.fromRGBO(249, 239, 238, 10),
-
+      backgroundColor: const Color.fromRGBO(249, 239, 238, 10),
       body: Stack(
           children:[
-            Center(
+            const Center(
               child:CircleAvatar(
                 backgroundImage: AssetImage('assets/masti.png'),
                 backgroundColor: Colors.transparent,
                 radius:40.0,
               ),
             ),
-            SizedBox(height: 40.0),
+            const SizedBox(height: 40.0),
             Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
@@ -83,27 +82,27 @@ class _ChatbotState extends State<Chatbot> {
     return Flexible(
       child: SingleChildScrollView(
         controller: _scrollController,
-        physics: BouncingScrollPhysics(),
+        physics: const BouncingScrollPhysics(),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
             ListView.builder(
               itemCount: messages.length,
               shrinkWrap:true,
-              padding: EdgeInsets.only(top: 10,bottom: 10),
-              physics: NeverScrollableScrollPhysics(),
+              padding: const EdgeInsets.only(top: 10,bottom: 10),
+              physics: const NeverScrollableScrollPhysics(),
               itemBuilder: (context, index){
                 return Container(
-                  padding: EdgeInsets.only(left: 14,right: 14,top: 10,bottom: 10),
+                  padding: const EdgeInsets.only(left: 14,right: 14,top: 10,bottom: 10),
                   child: Align(
                     alignment: (messages[index].messageType == "receiver"?Alignment.topLeft:Alignment.topRight),
                     child: Container(
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(20),
-                        color: (messages[index].messageType  == "receiver"?Colors.grey.shade200:Color.fromRGBO(227, 207, 201, 30)),
+                        color: (messages[index].messageType  == "receiver"?Colors.grey.shade200:const Color.fromRGBO(227, 207, 201, 30)),
                       ),
-                      padding: EdgeInsets.all(16),
-                      child: Text(messages[index].messageContent, style: TextStyle(fontSize: 15),),
+                      padding: const EdgeInsets.all(16),
+                      child: Text(messages[index].messageContent, style: const TextStyle(fontSize: 15),),
                     ),
                   ),
                 );
@@ -115,14 +114,14 @@ class _ChatbotState extends State<Chatbot> {
     );
   }
 
-  Widget bottomChatView() {
+ Widget bottomChatView() {
     return Container(
-      padding: EdgeInsets.only(left: 10, bottom: 10, top: 10),
+      padding: const EdgeInsets.only(left: 10, bottom: 10, top: 10),
       height: 60,
       width: double.infinity,
       child: Row(
         children: <Widget>[
-          SizedBox(
+          const SizedBox(
             width: 15,
           ),
           Expanded(
@@ -131,31 +130,30 @@ class _ChatbotState extends State<Chatbot> {
               onSubmitted: (String str) {
                 fetchFromDialogFlow(str);
               },
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                   hintText: "Write message...",
                   hintStyle: TextStyle(color: Colors.black54),
                   border: InputBorder.none),
             ),
           ),
-          SizedBox(
+          const SizedBox(
             width: 15,
           ),
           FloatingActionButton(
             onPressed: () {
               fetchFromDialogFlow(_inputMessageController.text);
             },
-            child: Icon(
-              Icons.send,
-              color: Colors.white,
-              size: 18,
-            ),
             backgroundColor: Colors.brown,
             elevation: 0,
+            child: const Icon(Icons.send, color: Colors.white,size: 18,),
           ),
         ],
       ),
     );
   }
+
+
+  // functions
 
   _scrollToBottom() {
     WidgetsBinding.instance.addPostFrameCallback((_) {
@@ -169,6 +167,7 @@ class _ChatbotState extends State<Chatbot> {
     AuthGoogle authGoogle =
     await AuthGoogle(fileJson: 'assets/chatbotkey.json').build();
     dialogflow = Dialogflow(authGoogle: authGoogle, language: Language.english);
+    fetchFromDialogFlow("hi");
   }
 
   fetchFromDialogFlow(String input) async {
@@ -176,11 +175,12 @@ class _ChatbotState extends State<Chatbot> {
     setState(() {
       messages.add(ChatMessage(messageContent: input, messageType: "sender"));
     });
-
     AIResponse response = await dialogflow.detectIntent(input);
-    print(response.getMessage());
-    messages.add(ChatMessage(
-        messageContent: response.getMessage(), messageType: "receiver"));
+    // print(response.getListMessage()[0]);
+    Map data =(response.getListMessage()[0]);
+    print(data['payload']['reply']);
+
+    messages.add(ChatMessage(messageContent: data['payload']['reply'], messageType: "receiver"));
     setState(() {});
   }
 }
