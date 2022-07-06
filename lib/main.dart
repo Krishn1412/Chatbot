5461a0e2e93c1c351bdc1a653415957eab830886
Krishn1412 <f20202093@hyderabad.bits-pilani.ac.in>
2022-07-06 18:38:03 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -53,29 +53,29 @@ class _ChatbotState extends State<Chatbot> {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(249, 239, 238, 10),
       body: Stack(
-          children:[
-            const Center(
-              child:CircleAvatar(
-                backgroundImage: AssetImage('assets/masti.png'),
-                backgroundColor: Colors.transparent,
-                radius:40.0,
+        children:[
+          const Center(
+            child:CircleAvatar(
+              backgroundImage: AssetImage('assets/masti.png'),
+              backgroundColor: Colors.transparent,
+              radius:40.0,
+            ),
+          ),
+          const SizedBox(height: 40.0),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              chatSpaceWidget(),
+              Container(
+                height: 1.0,
+                width: double.infinity,
+                color: Colors.blueGrey,
               ),
-            ),
-            const SizedBox(height: 40.0),
-            Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            chatSpaceWidget(),
-            Container(
-              height: 1.0,
-              width: double.infinity,
-              color: Colors.blueGrey,
-            ),
-            bottomChatView()
-          ],
-        ),
-      ],
-    ),
+              bottomChatView()
+            ],
+          ),
+        ],
+      ),
     );
   }
 
@@ -100,20 +100,21 @@ class _ChatbotState extends State<Chatbot> {
                     {fetchFromDialogFlow(messages[index].messageContent);}
                     if (messages[index].messageCategory == "activity") {
                       // redirect to pap activity
-                       print('redirected to pap');
-                      
+                      print('redirected to pap');
+
                     }
                   },
                   child: Container(
                     padding: const EdgeInsets.only(left: 14,right: 14,top: 10,bottom: 10),
                     child: Align(
-                      alignment: (messages[index].messageType == "receiver"?Alignment.topLeft:Alignment.topRight),
+                       alignment: (messages[index].messageType == "receiver"?Alignment.topLeft:Alignment.topRight),
+  
                       child: Container(
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(20),
                           color: (messages[index].messageType  == "receiver"?Colors.grey.shade200:(messages[index].messageCategory  == "suggestion"?const Color.fromARGB(225, 143, 164, 245)
-                                  :(messages[index].messageCategory  == "activity"? const Color.fromARGB(225, 171, 74, 45)
-                                      :const Color.fromRGBO(227, 207, 201, 30)))),
+                              :(messages[index].messageCategory  == "activity"? const Color.fromARGB(225, 171, 74, 45)
+                              :const Color.fromRGBO(227, 207, 201, 30)))),
                         ),
                         padding: const EdgeInsets.all(16),
                         child: Text(messages[index].messageContent, style: const TextStyle(fontSize: 15),),
@@ -129,7 +130,7 @@ class _ChatbotState extends State<Chatbot> {
     );
   }
 
- Widget bottomChatView() {
+  Widget bottomChatView() {
     return Container(
       padding: const EdgeInsets.only(left: 10, bottom: 10, top: 10),
       height: 60,
@@ -183,8 +184,8 @@ class _ChatbotState extends State<Chatbot> {
     await AuthGoogle(fileJson: 'assets/chatbotkey.json').build();
     dialogflow = Dialogflow(authGoogle: authGoogle, language: Language.english);
     messages.add(ChatMessage(
-        messageContent: "Hello from Masti !!! ", messageType: "receiver", messageCategory: "reply"));
-        var suggestions =["Suggestion 1", "Suggestion 2"];
+        messageContent: "Hello I am MASTHI, and I am here to help!! How are you feeling today?? ", messageType: "receiver", messageCategory: "reply"));
+    var suggestions =["Sad", "Tired","Worried", "Angry","Sleep"];
     for (var suggestion in suggestions) {
       messages.add(ChatMessage(
           messageContent: suggestion,
@@ -192,10 +193,7 @@ class _ChatbotState extends State<Chatbot> {
           messageCategory: "suggestion"));
     }
     setState(() {
-      
     });
-
-
   }
 
   fetchFromDialogFlow(String input) async {
@@ -205,24 +203,23 @@ class _ChatbotState extends State<Chatbot> {
     });
     AIResponse response = await dialogflow.detectIntent(input);
     Map data =response.getListMessage()[0];
+    messages.add(ChatMessage(
+        messageContent: data['payload']['reply'],
+        messageType: "receiver",
+        messageCategory: "reply"));
+    for (var suggestion in data['payload']['suggestions']) {
       messages.add(ChatMessage(
-          messageContent: data['payload']['reply'],
-          messageType: "receiver",
-          messageCategory: "reply"));
-      for (var suggestion in data['payload']['suggestions']) {
-        messages.add(ChatMessage(
-            messageContent: suggestion,
-            messageType: "sender",
-            messageCategory: "suggestion"));
-      }
-      for (var activity in data['payload']['activity']) {
-        messages.add(ChatMessage(
-            messageContent: activity,
-            messageType: "sender",
-            messageCategory: "activity"));
-      }
-       setState(() {});
+          messageContent: suggestion,
+          messageType: "sender",
+          messageCategory: "suggestion"));
+    }
+    for (var activity in data['payload']['activity']) {
+      messages.add(ChatMessage(
+          messageContent: activity,
+          messageType: "sender",
+          messageCategory: "activity"));
+    }
+    setState(() {});
   }
 }
-
 
